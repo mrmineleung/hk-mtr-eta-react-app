78322d3b42b7a5dde6452866d692d921d99d3630
Mine Leung <>
2023-08-14 17:51:40 -0400

    Add App.tsx

---
+++ src/App_old.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MTRNextTrain from './MTRNextTrain'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MTRNextTrain></MTRNextTrain>
+    </div>
+  );
+}
+
+export default App;
